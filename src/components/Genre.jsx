d2b0daf24739d13f1facd80c384e44e4bdf9efc1
KashurXpert <143515879+KashurXpert@users.noreply.github.com>
2023-08-29 19:06:25 +0530

    Update Genre.jsx

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -25,7 +25,7 @@ function Genre() {
     return (
         <>
         <Helmet>
-            <title>BlueBird Movies | Genres</title>
+            <title>Kashur Xpert</title>
         </Helmet>
 
         <div className='flex flex-wrap justify-center px-2'>
