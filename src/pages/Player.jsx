abae4c73feb8fe6c0926f3b7bee495adb909e11c
KashurXpert <143515879+KashurXpert@users.noreply.github.com>
2023-09-05 20:23:50 +0530

    Update Player.jsx

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -30,7 +30,7 @@ const Player = () => {
     return (
         <>
             <button onClick={()=>history.back()} className='fixed z-10 text-4xl text-black bg-white m-3 md:m-5 rounded-full'><HiChevronLeft /></button>
-            <iframe allowFullScreen style={{ display: 'flex', alignItems: "center", justifyContent: "center", width: "100%", height: "85vh" }} src={getSmashystreamUrl(id)}></iframe>
+            <iframe allowFullScreen style={{ display: 'flex', alignItems: "center", justifyContent: "center", width: "100%", height: "85vh", #ad-container {display: none !important;} }} src={getSmashystreamUrl(id)}></iframe>
         </>
     );
 }
