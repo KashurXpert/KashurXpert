c6dadf91f76f7e1dd777a298abde8e62c3791f1d
KashurXpert <143515879+KashurXpert@users.noreply.github.com>
2023-09-06 14:12:10 +0530

    Update Player.jsx

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useRef } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { getSmashystreamUrl, getSuperembedUrl, get2embedUrl } from '../movies';
 import { useState } from 'react';
@@ -9,6 +9,7 @@ const Player = () => {
     const { setHeader } = useContext(Contextpage);
     const [moviedet, setMoviedet] = useState([]);
     const { id } = useParams();
+    const iframeRef = useRef(null); // Reference to the iframe element
 
     const APIKEY = import.meta.env.VITE_API_KEY;
 
@@ -25,12 +26,35 @@ const Player = () => {
         setHeader("Player");
     }, []);
 
+    // When the iframe loads, hide the ad container
+    const handleIframeLoad = () => {
+        if (iframeRef.current) {
+            const iframeDocument = iframeRef.current.contentDocument || iframeRef.current.contentWindow.document;
+            const adContainer = iframeDocument.getElementById('ad-container');
+            if (adContainer) {
+                adContainer.style.display = 'none';
+            }
+        }
+    };
+
     document.title = `Kashur Xpert | ${moviedet.title}`;
 
     return (
         <>
-            <button onClick={()=>history.back()} className='fixed z-10 text-4xl text-black bg-white m-3 md:m-5 rounded-full'><HiChevronLeft /></button>
-            <iframe allowFullScreen style={{ display: 'flex', alignItems: "center", justifyContent: "center", width: "100%", height: "85vh", #ad-container {display: none !important;} }} src={getSmashystreamUrl(id)}></iframe>
+            <button onClick={() => history.back()} className='fixed z-10 text-4xl text-black bg-white m-3 md:m-5 rounded-full'><HiChevronLeft /></button>
+            <iframe
+                allowFullScreen
+                style={{
+                    display: 'flex',
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "100%",
+                    height: "85vh"
+                }}
+                src={getSmashystreamUrl(id)}
+                ref={iframeRef}
+                onLoad={handleIframeLoad} // Call the function when the iframe loads
+            ></iframe>
         </>
     );
 }
